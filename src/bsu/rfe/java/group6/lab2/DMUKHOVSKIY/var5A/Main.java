e47691b75389323078781e099d8762a990a701d2
Герман <DMUKHOVSKIY>
2021-10-30 19:26:43 +0300

    laba 2

---
+++ src/bsu/rfe/java/group6/lab2/DMUKHOVSKIY/var5A/Main.java
@@ -0,0 +1,13 @@
+package bsu.rfe.java.group6.lab2.DMUKHOVSKIY.var5A;
+
+import javax.swing.JFrame;
+
+public class Main {
+    // Главный метод класса
+    public static void main(String[] args) {
+        MainFrame frame = new MainFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+
+}
